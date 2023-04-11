eb2ed96cb023b795995ae37054c1bc38f944496a
Alex <serbaleks2001@gmail.com>
2023-04-11 17:00:21 +0300

    Initial commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Animal {
+    private String name;
+    public Animal(String name,int maxRunDistance, int maxSwimDistance) {
+        this.name = name;
+        this.maxRunDistance = maxRunDistance;
+        this.maxSwimDistance = maxSwimDistance;
+    }
+
+    private static int maxRunDistance;
+    private static int maxSwimDistance;
+    public void run(int distance){
+        if(maxRunDistance<=0){
+            System.out.println(name + " не може бігати!");
+        }else {
+            if (distance > maxRunDistance) {
+                System.out.println(name + " пробіг " + Integer.toString(maxRunDistance) + " та втомився");
+            } else System.out.println(name + " пробіг " + Integer.toString(distance));
+        }
+    }
+    public void swim(int distance){
+        if(maxSwimDistance<=0){
+            System.out.println(name + " не може плавати!");
+        }else {
+            if (distance > maxSwimDistance) {
+                System.out.println(name + " проплив " + Integer.toString(maxSwimDistance) + " після чого втонув(");
+            } else System.out.println(name + " проплив " + Integer.toString(distance));
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<>();
+        Cat cat1 = new Cat("Фелікс");
+        cat1.swim(150);
+        animals.add(cat1);
+        Cat cat2 = new Cat("Борис");
+        cat2.run(150);
+        animals.add(cat2);
+        Dog dog1 = new Dog("Шарик");
+        dog1.run(750);
+        animals.add(dog1);
+        Dog dog2 = new Dog("Мухтар");
+        dog2.swim(9);
+        animals.add(dog2);
+        Dog dog3 = new Dog("Бобік");
+        dog3.swim(11);
+        animals.add(dog3);
+
+        System.out.println("Усього тварин: "+ Integer.toString(animals.size()));
+        int dogs = 0;
+        int cats = 0;
+        for(int i=0;i<animals.size();i++){
+            if (animals.get(i).getClass()==Cat.class) cats++;
+            if (animals.get(i).getClass()==Dog.class) dogs++;
+        }
+        System.out.println("Собак: "+dogs+"\nКотів: "+cats);
+    }
+}
